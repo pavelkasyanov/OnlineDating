94df30229128ae18a23947078a6a70c91fdf37a2
Pavel_Kasyanov <paulkassianov@gmail.com>
2015-06-15 20:04:57 +0300

    change home controller

--- src/main/java/com/onlinedating/controller/HomeController.java
+++ src/main/java/com/onlinedating/controller/HomeController.java
@@ -1,22 +1,34 @@
 package com.onlinedating.controller;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(ModelMap model) {
+	public String index(ModelMap model, HttpServletRequest request) {
+		if (request.getSession().getAttribute("login_user") != null) {
+			return "home";
+		}
+
 		return "index";
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login(ModelMap model,
 						@RequestParam("login") String login,
-						@RequestParam("password") String pass) {
+						@RequestParam("password") String pass,
+						HttpServletRequest request) {
+
+		if (login != null || !login.equals("")) {
+			request.getSession().setAttribute("login_user", login);
+		}
 
 		return "redirect:/";
 	}
