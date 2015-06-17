c32309ff7fe43226ef3f3f62c95774b4a840e504
kirill <elteriar@mail.ru>
2015-06-17 15:54:23 +0300

    commit

---
+++ src/main/java/com/onlinedating/model/readConfFile.java
@@ -0,0 +1,14 @@
+package com.onlinedating.model;
+
+/**
+ * Created by Кирилл on 17.06.15.
+ */
+public class readConfFile {
+    public int[] getParametrs()
+    {
+        int[] parametrs = new int[12];
+
+        return parametrs;
+    }
+
+}
